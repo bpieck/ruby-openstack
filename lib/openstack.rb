fa15e416ad229396d303de7037dcd35ca7e46047
Bente Pieck <b.pieck@x-ion.de>
2014-08-25 23:53:12 +0200

    fixed limit since it is no q-query parameter

--- lib/openstack.rb
+++ lib/openstack.rb
@@ -118,29 +118,43 @@ module OpenStack
   end
 
   def self.get_ceilometer_query(params, ceilometer_keys, query_keys, url="")
-    query_keys = [query_keys] unless query_keys.is_a?(Array)
+    ceilometer_keys = [ceilometer_keys] unless ceilometer_keys.is_a?(Array)
+    keys = filter_keys(params, ceilometer_keys)
     if keys.empty?
       get_query_params(params, query_keys, url)
     else
-      keys = filter_keys(params, ceilometer_keys)
-      if query_keys.empty?
+      other_params = get_query_params(params, query_keys)
+      if other_params.empty?
         "#{url}?#{ceilometer_fields(keys)}&#{ceilometer_operations(keys)}&#{ceilometer_types(keys)}&#{ceilometer_values(keys, params)}"
       else
-        "#{get_query_params(params, query_keys, url)}&#{ceilometer_fields(keys)}&#{ceilometer_operations(keys)}&#{ceilometer_types(keys)}&#{ceilometer_values(keys, params)}"
+        "#{url}#{other_params}&#{ceilometer_fields(keys)}&#{ceilometer_operations(keys)}&#{ceilometer_types(keys)}&#{ceilometer_values(keys, params)}"
       end
     end
   end
 
   def self.ceilometer_values(keys, params)
-    keys.map { |key| "q.value=#{params[key]}" }.join('&')
+    keys.map { |key| "q.value=#{ceilometer_value_for params[key]}" }.join('&')
   end
 
   def self.ceilometer_fields(keys)
     keys.map { |key| "q.field=#{ceilometer_field_for key}" }.join('&')
   end
 
+  def self.ceilometer_types(keys)
+    keys.map { |key| "q.type=" }.join('&')
+  end
+
   def self.ceilometer_operations(keys)
     keys.map { |key| "q.op=#{ceilometer_operation_for key}" }.join('&')
+  end
+
+  def self.ceilometer_value_for(value)
+    case value
+      when Time
+        value.strftime('%Y-%m-%dT%H:%M:%S.%6N')
+      else
+        value
+    end
   end
 
   def self.ceilometer_operation_for(key)
